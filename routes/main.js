fd69afc4d023641dd4c3841d3d875d267207c4c3
youjin <youjinlee1997@gmail.com>
2021-01-06 16:56:48 +0900

    페이퍼 내 게시글 삭제 기능 추가

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const {User, Paper} = require('../models');
+const {User, Paper, Post} = require('../models');
 const {isLoggedIn, isNotLoggedIn, isItMe} = require('./middlewares');
 
 router.get('/', isLoggedIn, isItMe, async (req, res, next) => {
@@ -77,28 +77,50 @@ router.route('/:paper_id')
         const paper = await Paper.findOne({
             where: { userId: user_id, paper_id: paper_id }
         });
-        const user = await User.findOne({
-            where: {user_id}
+        const posts = await Post.findAll({
+            where: {'posts': paper_id},
         });
 
         if (master) { // 계정주인이 접근할때
-            return res.render('paper', {login: true, user, paper, master: true});
+            if (req.isAuthenticated() && (req.user.user_id == user_id)){
+                return res.render('paper', {login: true, user_id, paper, posts, master: true});
+            } else { //계정 주인이 아닌 사람이 접근할 때
+                const message = '접근 권한이 없습니다.';
+                return res.redirect(`/auth/login?loginError=${message}`);
+            }
         } else if (edit) {  // edit URL로 들어왔을 때
-            return res.render('paper', {login: true, user, paper, edit: true});
+            return res.render('paper', {login: true, user_id, paper, posts, edit: true});
         } else {  // 일반 url로 들어왔을 때(수정x)
-            return res.render('paper', {login: true, user, paper});
+            return res.render('paper', {login: true, user_id, paper, posts});
         }
     })
-    .post((req, res) => {
+    .post(async (req, res) => {
+        const {user_id, paper_id} = req.params;
         const {filter, deleting, edit} = req.query;
         if (filter) {
             console.log('필터링 단어 추가');
         }
         if (deleting) {
-            console.log('게시글 삭제');
+            const id = req.body.post_id;
+            await Post.destroy({
+                where: {id}
+            })
+            return res.redirect(`/${user_id}/${paper_id}?master=true`);
         }
         if (edit) {
-            console.log('게시글 작성');
+            const text = req.body.post;
+            Post.create({
+                text,
+                posts: paper_id
+            })
+            .then((post) => {
+                return res.redirect(`/${user_id}/${paper_id}?edit=true`);
+            })
+            .catch((err) => {
+                console.error(err);
+                const message = '롤링페이퍼를 작성할 수 없습니다';
+                return res.redirect(`/${user_id}/${paper_id}?edit=true?error=${message}`);
+            })
         }
     })
 router.get('/:paper_id/:post_id', (req, res) => {
